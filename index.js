b09eaf0560b00a1a2afd7a3c0cd95b00d161c03d
Lucas Stofaleti <lucastofaleti@hotmail.com>
2023-04-02 15:24:47 +0200

    feat: navbar & content

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function expandNavbar() {
   let element = document.querySelector(".outer-grid-expanded");
+  let nav = document.querySelector(".flex-nav-expanded");
   if (element) {
     element.classList.remove("outer-grid-expanded");
+    nav.classList.remove("flex-nav-expanded");
   } else {
     element = document.querySelector(".outer-grid");
+    nav = document.querySelector(".flex-nav");
     element.classList.add("outer-grid-expanded");
+    nav.classList.add("flex-nav-expanded");
   }
 }
